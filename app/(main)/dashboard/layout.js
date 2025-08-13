85fff0e079169639d3feb007b1b549b6e39a0c6c
Raushan Kumar <raushanahir0311@gmail.com>
2025-08-14 01:16:38 +0530

    first commit

---
+++ app/(main)/dashboard/layout.js
@@ -0,0 +1,15 @@
+import React, { Suspense } from "react";
+import Dashboard from "./page";
+import { BarLoader } from "react-spinners";
+
+
+export default function DashboardLayout(){
+    return (
+       <div className="px-5">
+        <h1 className="text-6xl font-bold gradient-title mb-5">Dashboard</h1>
+        <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}>
+        <Dashboard />
+        </Suspense>
+       </div>
+    );
+}
